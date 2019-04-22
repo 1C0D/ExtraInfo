267726a82f0a6d0ba2770fe7d38ebbe7674a9f45
daniel.grauer <grauer.d@gmail.com>
2019-04-22 18:22:45 +0200

    bumped version; dynamic offset; updated to new tool header; added information about selected objects; unregister added; lot of pep8 formating

--- ExtraInfo.py
+++ ExtraInfo.py
@@ -28,10 +28,10 @@ bl_info = {
     "name": "ExtraInfo",
     "description": "Show Extra Information in Viewport",
     "author": "zebus3d",
-    "version": (0, 0, 3),
+    "version": (0, 0, 4),
     "blender": (2, 80, 0),
     "location": "View3D",
-    "wiki_url": "https://github.com/zebus3d/ExtraInfo",
+    "wiki_url": "",
     "category": "3D View" 
 }
 
@@ -42,121 +42,31 @@ font_info = {
 }
 
 
-def get_region_property(target_region, target_property):
-    matched, i = False, 0
-    regions = list(bpy.context.area.regions)
-    region_property = None
-    while not matched:
-        if regions[i].type == target_region:
-            region_property = getattr(regions[i], target_property)
-            matched = True
-        i = i+1
-    
-    if region_property:
-        return region_property
-
-
-
-# this is calculated every drawing pass of the viewport:
 def draw_callback_px(self, context):
+    # esto se calcula cada pasada de dibujado del viewport:
 
     display = []
     font_id = font_info["font_id"]
     
-    ui_scale = bpy.context.preferences.view.ui_scale
+    uiScale = bpy.context.preferences.view.ui_scale
+    # print("uiscale: ", uiScale)
+    x_offset = 24 * uiScale + (bpy.context.area.regions[4].x - bpy.context.area.regions[1].x)   # regions (HUD-HEADER)
+    # for region in bpy.context.area.regions:
+    #     print('region type: ', region.type)
+    #     print('region x/y: ', region.x, ' / ', region.y)
+    #     print('region width/height: ', region.width, ' / ', region.height)
+    # print(40*"=")
 
-    x_offset_aTool = bpy.context.area.regions[2].width
+    y_offset = (bpy.context.area.regions[3].height + bpy.context.area.regions[4].y) - 110 * uiScale     # regions(UI+HUD)
+    # print("UI height: ", bpy.context.area.regions[2].height)
+    # print("hud y pos: ", bpy.context.area.regions[3].y)
+    # print("hud offset: ", bpy.context.area.regions[2].height + bpy.context.area.regions[3].y)
 
-    if x_offset_aTool == 1:
-        x_offset = 20 * ui_scale 
-    else:   
-        x_offset = 20 * ui_scale + x_offset_aTool
-
-    # esto es solo en el layaout principal pero puede cambiar:
-    # TOOL_HEADER = 0
-    # HEADER = 1
-    # TOOLS = 2
-    # UI = 3
-    # HUD = 4 
-    # WINDOW = 5
-    # por eso me hice el metodo get_region_property
-
-    header_height = get_region_property('HEADER', 'height')
-    header_y = get_region_property('HEADER', 'y')
-    
-    window_height = get_region_property('WINDOW', 'height')
-    # window_height = get_region_property('WINDOW', 'y')
-    # print("window.height", window_height)
-    
-    # normalize y offset:
-    # ui_min = 0.5
-    # ui_max = 2
-    # normalize range(0.5-2) to 0-1:
-    # y_normalized = (ui_scale - ui_min)/(ui_max-ui_min)
-    # print(y_normalized)
-    # normalize range(0.5-2) to 0-100:
-    # y_normalized = (ui_scale - ui_min)/(ui_max-ui_min)*100
-    # print(y_normalized)
-
-
-    if header_height == 1:
-        # sin header
-        # size min 0:
-        # y_static_offest = 35
-        # size 1:
-        # y_static_offest = 65
-        # size 2 max:
-        # y_static_offest = 130
-
-        OldMin = 0.5
-        OldMax = 2
-
-        NewMin = 35
-        NewMax = 130
-        
-        OldRange = (OldMax - OldMin)
-        # OldValue = y_normalized
-        OldValue = ui_scale
-
-        if OldRange == 0:
-            NewValue = NewMin
-        else:
-            NewRange = (NewMax - NewMin)  
-            y_static_offest = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
-    else:
-        # con header
-        # size min 0:
-        # y_static_offest = 50
-        # size 1:
-        # y_static_offest = 90        
-        # size 2 max:
-        # y_static_offest = 180
-        
-        OldMin = 0.5
-        OldMax = 2
-
-        NewMin = 50
-        NewMax = 180
-        
-        OldRange = (OldMax - OldMin)
-        # OldValue = y_normalized
-        OldValue = ui_scale
-
-        if OldRange == 0:
-            NewValue = NewMin
-        else:
-            NewRange = (NewMax - NewMin)  
-            y_static_offest = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
-
-    # print(y_static_offest)
-    y_offset = window_height - y_static_offest
-    # print("y_offset", y_offset)
-
-    fontSize = int(12 * ui_scale)
+    fontSize = int(12 * uiScale)
     blf.size(font_id, fontSize, 72)
     
-    # shadows:
-    # the level has to be 3, 5 o 0
+    # sombra:
+    # el level tiene que ser 3, 5 o 0
     level = 5
     r = 0.0
     g = 0.0
@@ -168,9 +78,9 @@ def draw_callback_px(self, context):
     blf.shadow_offset(font_id, 1, -1)
     
     engines = {
-        'BLENDER_EEVEE' : 'Eevee',
-        'BLENDER_WORKBENCH' : 'Workbench',
-        'CYCLES' : 'Cycles'
+        'BLENDER_EEVEE': 'Eevee',
+        'BLENDER_WORKBENCH': 'Workbench',
+        'CYCLES': 'Cycles'
     }
 
     re = 'Engine: ' + engines.get(bpy.context.scene.render.engine)
@@ -178,9 +88,13 @@ def draw_callback_px(self, context):
 
     view_layer = bpy.context.view_layer
     stats = bpy.context.scene.statistics(view_layer).split("|")
+    verts, polys, tris = selected_object_info(stats)
 
     if bpy.context.mode == 'OBJECT':
-        ss = stats[2:5]
+        ss = stats[5:5]
+        ss.insert(2, verts)
+        ss.insert(3, polys)
+        ss.insert(4, tris)
         ss.append(stats[-2])
         stats = ss
     elif bpy.context.mode == 'EDIT_MESH':
@@ -210,13 +124,38 @@ def draw_callback_px(self, context):
         if bpy.context.space_data.overlay.show_text:
             for counter, value in enumerate(display):
                 # print(value)
-                value = value.replace(" ","")
-                value = value.replace(":",": ")
+                value = value.replace(" ", "")
+                value = value.replace(":", ": ")
+                value = value.replace("/", " / ")
                 # print(value)
-                increment = (20*counter*ui_scale)
-                blf.position(font_id, x_offset, y_offset-increment-rendered*ui_scale, 0)
+                increment = (20*counter*uiScale)
+                blf.position(font_id, x_offset, y_offset-increment-rendered*uiScale, 0)
                 blf.draw(font_id, value)
 
+
+def selected_object_info(stats):
+    verts = 0
+    polys = 0
+    tris = 0
+
+    verts_scene = stats[2].split(':')[1]
+    polys_scene = stats[3].split(':')[1]
+    tris_scene = stats[4].split(':')[1]
+
+    for obj in bpy.context.selected_objects:
+        verts += len(obj.data.vertices)
+        polys += len(obj.data.polygons)
+
+        for face in obj.data.polygons:
+            vertices = face.vertices
+            triangles = len(vertices) - 2
+            tris += triangles
+
+    verts = '{}{}{}{}'.format('Verts:', polys, "/", verts_scene)
+    polys = '{}{}{}{}'.format('Faces:', polys, "/", polys_scene)
+    tris = '{}{}{}{}'.format('Tris:', tris, "/", tris_scene)
+    # print("selected mesh info: ", verts, polys, tris)
+    return verts, polys, tris
 
 
 def init():
@@ -228,8 +167,10 @@ def init():
 def register():
     init()
 
+
 def unregister():
-    pass
+    bpy.types.SpaceView3D.draw_handler_remove(font_info["handler"], 'WINDOW')
+
 
 if __name__ == "__main__":
     register()
